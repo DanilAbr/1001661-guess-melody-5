185b5ed3aad8a45e38009e254bea20e10b299567
Danila <Ltdfm@mail.ru>
2020-10-19 12:59:24 +0300

    Добавляет дополнительное действие

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -16,6 +16,8 @@ const reducer = (state = initialState, action) => {
       return extend(state, {
         step: state.step + action.payload,
       });
+    case ActionType.RESET_GAME:
+      return extend({}, initialState);
   }
 
   return state;
