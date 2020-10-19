67a3b5bb90114cd5f656cdc52c5cf7231c2f7fbd
Danila <Ltdfm@mail.ru>
2020-10-19 23:57:53 +0300

    Добавляет компонент Mistakes

---
+++ src/components/mistakes/mistakes.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Mistakes = (props) => {
+  const {count} = props;
+
+  const mistakes = Array.from({length: count});
+  console.log(mistakes);
+
+  return (
+    <div className="game__mistakes">
+      {mistakes.map((mistake, index) =>
+        <div
+          key={`mistake-${index}`}
+          className="wrong" />
+      )}
+    </div>
+  );
+};
+
+
+Mistakes.propTypes = {
+  count: PropTypes.number.isRequired,
+};
+
+
+export default Mistakes;
