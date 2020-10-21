1a5b9ca382311720edccaaa84fcb4324fc491ad9
Danila <Ltdfm@mail.ru>
2020-10-21 17:49:29 +0300

    Добавляет действия для экранов проигрыша и победы

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
+import {MAX_MISTAKE_COUNT} from '../../const';
 import WelcomeScreen from '../welcome-screen/welcome-screen';
 import AuthScreen from '../auth-screen/auth-screen';
 import GameOverScreen from '../game-over-screen/game-over-screen';
 import WinScreen from '../win-screen/win-screen';
 import GameScreen from '../game-screen/game-screen';
-import {MAX_MISTAKE_COUNT} from '../../const';
 
 
 const App = () => {
