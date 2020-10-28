e893f8133fbe8cc9803113ced7f3749eb1ada62c
Danila <Ltdfm@mail.ru>
2020-10-28 12:17:51 +0300

    Устанавливает пакет history

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -2,25 +2,25 @@ import {loadQuestions, requireAuthorization} from './action';
 import {AuthorizationStatus} from '../const';
 
 
-const fetchQuestionList = () => (dispatch, _getState, api) => {
+const fetchQuestionList = () => (dispatch, _getState, api) => (
   api.get(`/questions`)
-    .then(({data}) => dispatch(loadQuestions(data)));
-};
+    .then(({data}) => dispatch(loadQuestions(data)))
+);
 
 
-const checkAuth = () => (dispatch, _getState, api) => {
+const checkAuth = () => (dispatch, _getState, api) => (
   api.get(`/login`)
     .then(() => dispatch(requireAuthorization(AuthorizationStatus.AUTH)))
     .catch((err) => {
       throw err;
-    });
-};
+    })
+);
 
 
-const login = ({login: email, password}) => (dispatch, _getState, api) => {
+const login = ({login: email, password}) => (dispatch, _getState, api) => (
   api.post(`/login`, {email, password})
-    .then(() => dispatch(requireAuthorization(AuthorizationStatus.AUTH)));
-};
+    .then(() => dispatch(requireAuthorization(AuthorizationStatus.AUTH)))
+);
 
 
 export {fetchQuestionList, checkAuth, login};
